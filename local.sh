e9ef7d134b65601d631b70a66d9b6e3ed5f75bf3
Anmol Sharma <anmolsharma0234@gmail.com>
2024-05-26 23:21:28 +0530

    feat: week 5 challenge
    Signed-off-by: Anmol Sharma <anmolsharma0234@gmail.com>

---
+++ local.sh
@@ -0,0 +1,20 @@
+npm install
+
+# Copy data directory to a temporary directory
+cp -r ./data ./tmp-data
+
+# Spawn Bitcoind, and provide execution permission.
+docker compose up -d
+chmod +x ./bash/run-bash.sh
+chmod +x ./python/run-python.sh
+chmod +x ./javascript/run-javascript.sh
+chmod +x ./rust/run-rust.sh
+chmod +x ./run.sh
+
+# Run the test scripts
+/bin/bash run.sh
+npm run test
+docker compose down -v
+
+# Remove the temporary directory
+rm -rf ./tmp-data
